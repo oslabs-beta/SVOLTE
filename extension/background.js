75f9c710fb8cf34c37246b215156dd051fe1ca4a
Ying-An <yingan.wang@berkeley.edu>
2023-05-10 18:39:17 -0700

    trying to connect content script with background

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,21 @@
-console.log('hello');
-
+console.log(chrome.runtime.id);
 // chrome.runtime.onInstalled.addListener(()=>{
 //   console.log('hello');
-
 // })
 
-// chrome.runtime.onMessage.addListener((message,sender,res)=>{
-//   console.log(message, sender);
-//   res({response:'Bye'});
-// })
+chrome.runtime.onMessage.addListener((message,sender,res)=>{
+  console.log(sender.tab ?
+      "from a content script: " + sender.tab.url :
+      "from the extension");
+  console.log(message, sender);
+  res({farewell:'bye - from background'});
+})
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//       "from a content script: " + sender.tab.url :
+//       "from the extension");
+//     if (request.greeting === "HELLO") sendResponse({ farewell: "GOODBYE" });
+//   }
+// );
--- extension/content.js
+++ extension/content.js
@@ -1,12 +1,31 @@
-let code = `
-  window.document.addEventListener('SvelteRegisterComponent', (e) => {
-    console.log(e.detail);
-  })`
-  
+chrome.runtime.connect('kddekcfhbcgaobhfpgfidbjgdjkaobkn');
 
-  chrome.runtime.sendMessage({payload: 'Hello' },(res)=>{
-    console.log(res);
-  });
-// chrome.devtools.inspectedWindow.eval('console.log("yoo!")')
-document.documentElement.setAttribute('onreset', code);
-document.documentElement.dispatchEvent(new CustomEvent('reset')); 
+let counter = 0;
+const id = chrome.runtime.id; 
+console.log('chrome.runtime is ', chrome.runtime);
+
+console.log('id is ', id);
+
+
+// kddekcfhbcgaobhfpgfidbjgdjkaobkn
+
+const componentsObj = new Proxy({}, {
+  set : function (target, key, val) {
+    target[key] = val;
+    console.log(componentsObj);
+    chrome.runtime.sendMessage('kddekcfhbcgaobhfpgfidbjgdjkaobkn',{ message : 'proxy obj is being changed' });
+  }
+})
+
+window.document.addEventListener('SvelteRegisterComponent', (e) => {
+  chrome.runtime.sendMessage('kddekcfhbcgaobhfpgfidbjgdjkaobkn',{ message : 'sending from event listener' });
+  componentsObj[counter] = e.detail;
+  counter++
+});
+
+// caught TypeError: Error in invocation of runtime.sendMessage(optional string extensionId, any message, optional object options, optional function callback): chrome.runtime.sendMessage() called from a webpage must specify an Extension ID (string) for its first argument.
+ 
+
+// chrome.runtime.sendMessage({greeting: "HELLO"}, function(response) {
+//   console.log(response.farewell);
+// });
