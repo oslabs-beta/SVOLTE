03505f178b033122a4014664a020ca0bb7278582
Huy Vu <huynvu@live.com>
2023-05-10 21:00:53 -0700

    add connection between content,iso,and background, needs work on component destructuring

--- extension/background.js
+++ extension/background.js
@@ -3,13 +3,14 @@ console.log(chrome.runtime.id);
 //   console.log('hello');
 // })
 
-chrome.runtime.onMessage.addListener((message,sender,res)=>{
-  console.log(sender.tab ?
-      "from a content script: " + sender.tab.url :
-      "from the extension");
-  console.log(message, sender);
-  res({farewell:'bye - from background'});
-})
+chrome.runtime.onMessage.addListener((message, sender, res) => {
+  console.log(
+    sender.tab
+      ? "from a content script: " + sender.tab.url
+      : "from the extension"
+  );
+  res({ crikey: "m8" });
+});
 
 // chrome.runtime.onMessage.addListener(
 //   function(request, sender, sendResponse) {
@@ -19,3 +20,23 @@ chrome.runtime.onMessage.addListener((message,sender,res)=>{
 //     if (request.greeting === "HELLO") sendResponse({ farewell: "GOODBYE" });
 //   }
 // );
+
+// chrome.runtime.onConnect.addListener(function (port) {
+//   console.log("On connect add listener");
+//   const extensionListener = function (message, sender, sendResponse) {
+//     // The original connection event doesn't include the tab ID of the
+//     // DevTools page, so we need to send it explicitly.
+//     if (message.name == "init") {
+//       connection = port;
+//       return;
+//     }
+//     // other message handling
+//   };
+//   // Listen to messages sent from the DevTools page
+//   port.onMessage.addListener(extensionListener);
+//   // handle disconnect
+//   port.onDisconnect.addListener(function (port) {
+//     port.onMessage.removeListener(extensionListener);
+//     connection = null;
+//   });
+// });
--- extension/content.js
+++ extension/content.js
@@ -1,31 +1,19 @@
-chrome.runtime.connect('kddekcfhbcgaobhfpgfidbjgdjkaobkn');
+// const componentsObj = new Proxy(
+//   {},
+//   {
+//     set: function (target, key, val) {
+//       target[key] = val;
 
-let counter = 0;
-const id = chrome.runtime.id; 
-console.log('chrome.runtime is ', chrome.runtime);
+//     },
+//   }
+// );
 
-console.log('id is ', id);
-
-
-// kddekcfhbcgaobhfpgfidbjgdjkaobkn
-
-const componentsObj = new Proxy({}, {
-  set : function (target, key, val) {
-    target[key] = val;
-    console.log(componentsObj);
-    chrome.runtime.sendMessage('kddekcfhbcgaobhfpgfidbjgdjkaobkn',{ message : 'proxy obj is being changed' });
-  }
-})
-
-window.document.addEventListener('SvelteRegisterComponent', (e) => {
-  chrome.runtime.sendMessage('kddekcfhbcgaobhfpgfidbjgdjkaobkn',{ message : 'sending from event listener' });
-  componentsObj[counter] = e.detail;
-  counter++
+window.document.addEventListener("SvelteRegisterComponent", (e) => {
+  console.log(e.detail.component);
+  console.log(e.detail.component.toString());
+  const msg = e.detail.component.toString();
+  window.postMessage({
+    msg: msg,
+    source: "content.js",
+  });
 });
-
-// caught TypeError: Error in invocation of runtime.sendMessage(optional string extensionId, any message, optional object options, optional function callback): chrome.runtime.sendMessage() called from a webpage must specify an Extension ID (string) for its first argument.
- 
-
-// chrome.runtime.sendMessage({greeting: "HELLO"}, function(response) {
-//   console.log(response.farewell);
-// });
