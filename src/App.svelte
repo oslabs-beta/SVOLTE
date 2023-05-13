5c9e18461c8f3358597e2db5dc003b102baf99ab
Ying-An <yingan.wang@berkeley.edu>
2023-05-13 16:34:04 -0700

    configuring store and file structure

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,34 @@
   import Counter from './components/Counter.svelte'
   import Header from './components/Header.svelte'
   import Body from './components/Body.svelte'
-  let url = window.location.pathname;
-  const setUrl = () =>{
-    url = window.location.pathname;
-  }
+  import { path, setPath } from './stores.js'
+
+  // sveltekit routing
+  // let url = window.location.pathname;
+  // const setUrl = () =>{
+  //   url = window.location.pathname;
+  // }
+
+  // let path = 'tree';
+  // const setPath = () => {
+  //   if (path === 'tree') path = 'time';
+  //   else path = 'tree';
+  // }
+
 </script>
 
 <main>
-  <Header setUrl={setUrl}/>
+  <!-- <Header setUrl={setUrl}/> -->
   <div class="card">
     <Counter />
   </div>
-  <Body url={url}/>
+  <!-- <Body url={url}/> -->
+  <button on:click={setPath}>switch view</button>
+  {#if path === 'tree'}
+    <h1> currently at tree </h1>
+    {:else if path === 'time'}
+    <h1> currently at time travel</h1>
+  {/if}
 </main>
 
 <style>
