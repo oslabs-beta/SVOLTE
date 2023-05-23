f5ffdf64d0e11ef32644e16f3834b30c5306a63c
Scott Kim <skottcim@gmail.com>
2023-05-22 17:09:01 -0700

    HUGEEEE PROGRESS: successfully grab every node and send it from main world > isolated world > background > devtool and then reconstructed the root structure on the devtool side inside store

--- extension/isolated.js
+++ extension/isolated.js
@@ -8,9 +8,8 @@ window.addEventListener("message", (msg) => {
   ) {
     return;
   } else {
-    console.log("Source: isolated.js - message received: ", msg.data);
     chrome.runtime.sendMessage(msg.data, (res) => {
-      console.log(res);
+      return
     });
   }
 });
