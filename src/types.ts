4a9caef9d65dfa4312016e937453c3958b36e970
Huy Vu <huynvu@live.com>
2023-05-25 17:04:14 -0700

    add diff and snapshothistory and time components

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,33 @@
-interface Message {
-  id:number,
-  type:string,
-  target:object,
+export interface Message {
+  type: string;
+  source: string;
+  anchor?: number;
+  target?: number;
+  node: Node;
+}
+export interface Node {
+  id: number;
+  children: Array<Node> | [];
+  parent: Node | null;
+  invalidate?: () => void;
+  _timeout?: number;
+  tagName: string;
+  type: string;
+  detail?: {
+    attributes: any[];
+    listeners: [];
+    ctx: any[];
+  };
+}
+export interface SnapShot {
+  id: string;
+  tagName: string;
+  type: string;
+  diff: Array<Difference>;
+}
 
-
+interface Difference {
+  path: Array<string>;
+  value1: any;
+  value2: any;
 }
