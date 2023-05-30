c26a64ac64dd485139d875fccf9aa91ddc567688
PetrosO123 <pmo20@miami.edu>
2023-05-30 13:25:32 -0400

    UI component tree bug fixes, stylistic details

--- extension/devtools/index.js
+++ extension/devtools/index.js
@@ -3,11 +3,6 @@ This script is run whenever the devtools are open.
 In here, we can create our panel.
 */
 
-chrome.devtools.panels.create('SVOLTE', null, './devtools/panel/panel.html', (panel) =>{
-    panel.onShown.addListener(()=> {
-      chrome.devtools.inspectedWindow.reload();
-    })
-
-  });
+chrome.devtools.panels.create('SVOLTE', null, './devtools/panel/panel.html');
 
 
