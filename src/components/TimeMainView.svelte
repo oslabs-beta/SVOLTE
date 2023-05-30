9f84e70a1453d73d85f7cc172caa8d09c1226719
Huy Vu <huynvu@live.com>
2023-05-30 12:04:51 -0700

    add visualization for nested state obj in timemainview

--- src/components/TimeMainView.svelte
+++ src/components/TimeMainView.svelte
@@ -9,13 +9,27 @@
   }
 </script>
 
-<div class="h-full grow items-center content-center flex-col flex">
-  {#if state.length}
+{#if state.length}
+  <div class="h-full grow flex flex-col">
     {#each state as pair}
-      <div>{pair.key}: {pair.value}</div>
+      <div class="w-fit text-left self-center">
+        {pair.key}: {#if !Array.isArray(pair.value) && typeof pair.value === 'object'}
+          <span>
+            {`{`}
+            {#each Object.keys(pair.value) as key}
+              <div class="w-fit text-left">
+                {key}:{JSON.stringify(pair.value[key])},
+              </div>
+            {/each}
+            {`}`}
+          </span>
+        {:else}
+          {JSON.stringify(pair.value)}
+        {/if}
+      </div>
     {/each}
-  {/if}
-</div>
+  </div>
+{/if}
 
 <style>
 </style>
