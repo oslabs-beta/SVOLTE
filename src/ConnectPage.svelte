96d20177483984cc78972d8fa441e42c0f823ae9
Scott Kim <skottcim@gmail.com>
2023-05-30 11:49:27 -0700

    styled connect page, added banner to main dev tool, changed colors for component tree

--- src/ConnectPage.svelte
+++ src/ConnectPage.svelte
@@ -3,8 +3,27 @@
 
 </script>
 
-<div>
-  <h1>Welcome to Svolte!</h1>
-  <p>To begin, click connect to refresh the page.</p>
-  <button on:click={reload}>CONNECT</button>
+<div class="center bg-gradient-to-t from-gray-800 to-slate-800 ...">
+  <img src="/images/connectpage.png" alt="svolte connect page"/>
+  <p class="text-xl">To begin, click connect to refresh the page.</p>
+  <button type="button" class="btn variant-filled sm" on:click={reload}>Connect</button>
 </div>
+
+<style>
+  .center {
+		text-align: center;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+	}
+  img {
+    max-width: 50%;
+    width: 60%;
+    height: auto;
+  }
+</style>
+
+
+<button type="button" class="btn variant-filled">Connect</button>
